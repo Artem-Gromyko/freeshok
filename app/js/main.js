7a124f9ad222001dea344456e569d95b9a96af6b
Artem-Gromyko <gromykoartem48@gmail.com>
2021-11-29 23:18:11 +0600

    added adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -12,18 +12,46 @@ $(function () {
         dots: true,
       }
     }],
-    
+
     prevArrow: '<button type="button" class="top-slider__arrow top-slider__arrow--prev"><svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="60" rx="6" fill="white" fill-opacity="0.8"/><path d="M21.0382 31.0149L37.0515 45.6079C37.577 46.1307 38.4292 46.1307 38.9546 45.6079C39.4801 45.0851 39.4801 44.2369 38.9546 43.7141L23.9069 30L38.9533 16.2859C39.4788 15.7631 39.4788 14.9148 38.9533 14.3921C38.4278 13.8693 37.5756 13.8693 37.0502 14.3921L21.0369 28.9851C20.7568 29.2638 20.6367 29.6332 20.6554 29.9986C20.638 30.3654 20.7581 30.7348 21.0382 31.0149Z" fill="#505050"/></svg></button >',
     nextArrow: '<button type="button" class="top-slider__arrow top-slider__arrow--next"><svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="60" rx="6"	fill="white" fill-opacity="0.8"/><path d="M38.9641 28.9851L22.9508 14.3921C22.4254 13.8693 21.5731 13.8693 21.0477 14.3921C20.5222 14.9149 20.5222 15.7631 21.0477 16.2859L36.0954 30L21.049 43.7141C20.5235 44.2369 20.5235 45.0852 21.049 45.6079C21.5745 46.1307 22.4267 46.1307 22.9521 45.6079L38.9654 31.0149C39.2455 30.7362 39.3656 30.3668 39.3469 30.0014C39.3643 29.6346 39.2443 29.2652 38.9641 28.9851Z" fill="#505050"/></svg></button>',
   });
 
 
-  // $('.partners__items').slick({
-  //   dots: false,
-  //   arrows: false,
-  //   slidesToShow: 6,
-  //   slidesToScroll: 1,
-  // });
+  $('.partners-slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      }
+    ],
+
+    prevArrow: '<button type="button" class="partners-slider__arrow partners-slider__arrow--prev"><svg width="30" height="30" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16 7H3.8L9.4 1.4L8 0L0 8L8 16L9.4 14.6L3.8 9H16V7Z"fill="#63A60F"/></svg></button>',
+
+    nextArrow: '<button type="button" class="partners-slider__arrow partners-slider__arrow--next"><svg width="30" height="30" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 7H12.2L6.6 1.4L8 0L16 8L8 16L6.6 14.6L12.2 9H0V7Z" fill="#63A60F"/></svg></button >',
+  });
 
 
   $('.menu__btn').on('click', function () {
